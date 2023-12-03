198d0c9ead7d7e7bca410f0bde496d6587c8969b
Dann Wee <dannweezj@gmail.com>
2023-12-03 16:28:02 +0700

    setup home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,79 @@
-export default function Home() {
-  return (
-    <main>
+"use client";
 
-    </main>
-  )
+import TypewriterTitle from "@/components/main/TypewriterTitle";
+import { FileText, Github } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { HashLoader } from "react-spinners";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleEnterChainIn = () => {
+    router.push("/dashboard/home");
+  };
+
+  return (
+    <div className="w-screen h-screen circles">
+      <nav className="topbar py-8 px-5">
+        <Link href="/" className="absolute">
+          <Image
+            src="/assets/chainin-logo-no-bg.png"
+            alt="logo"
+            width={210}
+            height={210}
+          />
+        </Link>
+        <div className="flex items-center gap-1">
+          <div className="block md:hidden"></div>
+        </div>
+
+        <div>
+          <div className="flex items-center gap-7 pr-8">
+            <Link
+              href="https://elements.getpostman.com/redirect?entityId=31443216-dd0d4c78-1b59-4c28-9376-7605289e74b8&entityType=collection"
+              className="bg-[#E6E6E6] p-3 rounded-2xl hover:bg-[#6789bA]  hover:text-[#FFFFFF]"
+            >
+              <FileText className="cursor-pointer w-6 h-6" />
+            </Link>
+            <Link
+              href="https://github.com/usechainin"
+              className="bg-[#E6E6E6] p-3 rounded-2xl hover:bg-[#6789bA] hover:text-[#FFFFFF]"
+            >
+              <Github className="cursor-pointer w-6 h-6" />
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+        {loading ? (
+          <h1 className="font-semibold text-5xl text-center blue-text-gradient">
+            <HashLoader
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full"
+              color="000000"
+            />
+          </h1>
+        ) : (
+          <div>
+            {" "}
+            <h1 className="font-semibold text-6xl text-center">ChainIn</h1>
+            <div className="mt-4"></div>
+            <h2 className="font-semibold text-4xl text-center">
+              Business & Employment-Focused dApp
+            </h2>
+            <div className="mt-4"></div>
+            <h3 className="font-semibold text-2xl text-center text-black">
+              <TypewriterTitle />
+            </h3>
+            <div className="mt-4"></div>
+            <div className="flex justify-center"></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- components/main/TypewriterTitle.tsx
+++ components/main/TypewriterTitle.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import Typewriter from 'typewriter-effect'
+import React from "react";
+import Typewriter from "typewriter-effect";
 
-type Props = {}
+type Props = {};
 
 const TypewriterTitle = (props: Props) => {
-    return (
-        <Typewriter 
-            options={{
-                loop: true, // loop indefinitely
-            }}
-            onInit={(typewriter) => {
-                typewriter
-                .typeString("✅ Connecting Anonymous Users to Communities")
-                .pauseFor(250).deleteAll()
-                .typeString("✅ Powered by Simso Connect and Tableland")
-                .pauseFor(250).deleteAll()
-                .typeString("✅ Leveraging ZK Proofs for Ultimate Discretion")
-                .pauseFor(250).deleteAll()
-                .start();
-            }}
-        />
-    )
-    }
+  return (
+    <Typewriter
+      options={{
+        loop: true, // loop indefinitely
+      }}
+      onInit={(typewriter) => {
+        typewriter
+          .typeString("✅ Decentralising Careers, Connecting Futures")
+          .pauseFor(250)
+          .deleteAll()
+          .typeString(
+            "✅ Powered by Chainlink CCIP, Polygon ID, The Graph and Tableland"
+          )
+          .pauseFor(250)
+          .deleteAll()
+          .typeString(
+            "✅ Leveraging Blockchain to Create a New Paradigm for Careers"
+          )
+          .pauseFor(250)
+          .deleteAll()
+          .start();
+      }}
+    />
+  );
+};
 
-export default TypewriterTitle
+export default TypewriterTitle;
