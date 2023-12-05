ca10c5babb369f0c58efbb26bbca8ced078d695a
EndlessLucky <105.olivesoft@gmail.com>
2023-12-05 00:27:23 -0500

    feat - working on createOrg function

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,7 +1,23 @@
+'use client';
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { ethers } from "ethers";
 
-const page = () => {
-  return <div></div>;
+import deployerABI from "../../../contracts/ABI/Deployer.json";
+
+export default function page () {
+  const createOrg = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(
+      `https://polygon-mumbai.g.alchemy.com/v2/JN27FrWzaeP_d2e4vtzLVdhD8opPD-xS`
+    );
+    const deployerAddr = "0x0854d10eF62823731C0227670915C9F569f8f47f"; // deployer contract on mumbai
+  }
+
+  return ( 
+    <div>
+      <Button onClick={createOrg}>
+        Create an Organization
+      </Button>
+    </div>
+  )
 };
-
-export default page;
