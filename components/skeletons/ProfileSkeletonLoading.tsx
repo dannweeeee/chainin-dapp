f794f386140cc43254d63995e846340d5be62257
Dann Wee <dannweezj@gmail.com>
2023-12-06 20:36:00 +0700

    update profile skeleton loading

--- components/skeletons/ProfileSkeletonLoading.tsx
+++ components/skeletons/ProfileSkeletonLoading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProfileSkeletonLoading() {
   return (
     <div>
-      <div className="bg-[#6789BA] shadow rounded-xl p-2 w-full">
+      <div className="bg-[#8AA4CA] rounded-xl p-2 w-full">
         <div className="animate-pulse flex gap-3">
           <div className="rounded-lg bg-[#EDEDE9] h-[80px] w-[80px]"></div>
           <div className="flex-1 space-y-2 py-3 ">
