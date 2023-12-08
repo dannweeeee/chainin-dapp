741201a17cf7c182c0d3c8626741bd4d9e5a0dd0
Dann Wee <dannweezj@gmail.com>
2023-12-08 17:53:34 +0700

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,27 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { RainbowKitProviders } from "@/components/provider/rainbowkit-provider";
 import "@rainbow-me/rainbowkit/styles.css";
-import '@radix-ui/themes/styles.css';
+import "@radix-ui/themes/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ChainIn",
-  description: "A Business & Employment-Focused dApp",
+  description:
+    "Authenticity-Driven, Privacy-Focused Business & Employment Aggregator dApp",
+  keywords: [
+    "Authenticity-Driven",
+    "Privacy-Focused",
+    "Blockchain",
+    "dApp",
+    "Chainlink",
+    "Tableland",
+    "The Graph",
+    "Polygon ID",
+  ],
+  openGraph: {
+    images: "/assets/chainin-logo.png",
+  },
 };
 
 export default function RootLayout({
