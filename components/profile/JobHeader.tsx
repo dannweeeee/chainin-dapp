ee7f96570d999806525e1416fc3f1e45e6650e8f
zengzengzenghuy <tayzh2799@gmail.com>
2023-12-09 17:51:43 +0800

    fix merge conflict

--- components/profile/JobHeader.tsx
+++ components/profile/JobHeader.tsx
@@ -12,14 +12,9 @@ import {
   usePrepareContractWrite,
   useContractWrite,
   useWaitForTransaction,
+  sepolia,
 } from "wagmi";
 
-import { useQuery } from "react-query";
-import { GraphQLClient, gql } from "graphql-request";
-
-import ApplyJobCard from "../cards/ApplyJobCard";
-import { subgraphEndpoints } from "../../constants/index";
-import { returnDestinationInfo } from "../../lib/utils";
 import {
   Dialog,
   DialogClose,
@@ -30,6 +25,14 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import { SyncLoader } from "react-spinners";
+
+import { useQuery } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+import ApplyJobCard from "../cards/ApplyJobCard";
+import { subgraphEndpoints } from "../../constants/index";
+import { returnDestinationInfo } from "../../lib/utils";
+import { list } from "postcss";
 
 interface Props {
   listing_id: number;
@@ -69,23 +72,7 @@ interface OrgDetails {
     website_url: string;
   }[];
 }
-interface OrgDetails {
-  meta: {
-    duration: number;
-  };
-  success: boolean;
-  results: {
-    creator_wallet_address: string;
-    description: string;
-    nft_contract_address: string;
-    organisation_id: number;
-    organisation_name: string;
-    organisation_symbol: string;
-    organisation_type: number;
-    picture_url: string;
-    website_url: string;
-  }[];
-}
+
 interface Applicant {
   id: string;
   applicant: string;
@@ -102,9 +89,30 @@ interface ListingIDCounts {
   [listingID: string]: number;
 }
 
-function fetchAllApplicant({ listing_id }: Props) {}
-function postNewApplicant(originalData: Applicant[], newData: Applicant[]) {
-  if (originalData) {
+interface subgraphReturnedData {
+  combinedData: Applicant[];
+  sepoliaCount: number;
+  optimismCount: number;
+  fujiCount: number;
+}
+
+function postNewApplicant(
+  originalData: Applicant[] | undefined,
+  newData: Applicant[]
+) {
+  if (originalData == undefined) {
+    // POST all data to database
+    newData.forEach(async (applicant) => {
+      const data = await ChainInApi.createApplication(
+        applicant.id,
+        applicant.applicant,
+        Number(applicant.listingID),
+        applicant.profileURL
+      );
+      console.log("POST Application data ", data);
+    });
+  } else if (originalData) {
+    // find new applicant and post to database
     const newEntries = newData.filter((newEntry) => {
       return !originalData!.some((prevEntry) => prevEntry.id === newEntry.id);
     });
@@ -121,17 +129,9 @@ function postNewApplicant(originalData: Applicant[], newData: Applicant[]) {
       console.log("POST Application data ", data);
     });
   }
-
-  // Update previousDataArray for the next comparison
 }
-function JobHeader({ listing_id }: Props) {
-  const [jobData, setJobData] = useState<JobDetails | null>(null);
-  const [orgData, setOrgData] = useState<OrgDetails | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [totalApplicantCount, setTotalApplicantCount] = useState(0);
-  const router = useRouter();
-  const { address, status } = useAccount();
 
+async function fetchAllApplicantFromSubgraph({ listing_id }: Props) {
   const sepoliaGraphQLClient = new GraphQLClient(
     returnDestinationInfo("Sepolia").subgraph
   );
@@ -153,6 +153,77 @@ function JobHeader({ listing_id }: Props) {
       }
     }
   `;
+
+  let sepoliaResponse: subgraphResponse = await sepoliaGraphQLClient.request(
+    queryFormat
+  );
+  let opResponse: subgraphResponse = await opGoerGraphQLClient.request(
+    queryFormat
+  );
+  let fujiResponse: subgraphResponse = await fujiGraphQLClient.request(
+    queryFormat
+  );
+
+  console.log("sepolia response ", sepoliaResponse);
+  console.log("op response ", opResponse);
+  console.log("fuji response ", fujiResponse);
+  const sepoliaApplicants = sepoliaResponse.data.newApplicants;
+
+  const sepoliaListingIDCount: ListingIDCounts = {};
+
+  sepoliaApplicants.forEach((applicant) => {
+    const listingID = applicant.listingID;
+    sepoliaListingIDCount[listingID] =
+      (sepoliaListingIDCount[listingID] || 0) + 1;
+  });
+
+  const fujiApplicants = fujiResponse.data.newApplicants;
+
+  const fujiListingIDCount: ListingIDCounts = {};
+
+  fujiApplicants.forEach((applicant) => {
+    const listingID = applicant.listingID;
+    fujiListingIDCount[listingID] = (fujiListingIDCount[listingID] || 0) + 1;
+  });
+
+  const opApplicants = opResponse.data.newApplicants;
+
+  const opListingIDCount: ListingIDCounts = {};
+
+  opApplicants.forEach((applicant) => {
+    const listingID = applicant.listingID;
+    opListingIDCount[listingID] = (opListingIDCount[listingID] || 0) + 1;
+  });
+
+  let combinedApplicantData = [
+    ...sepoliaApplicants,
+    ...fujiApplicants,
+    ...opApplicants,
+  ];
+
+  const returnData = {
+    combinedData: combinedApplicantData,
+    sepoliaCount: sepoliaListingIDCount[listing_id],
+    optimismCount: opListingIDCount[listing_id],
+    fujiCount: fujiListingIDCount[listing_id],
+  };
+  return returnData;
+}
+function JobHeader({ listing_id }: Props) {
+  const [jobData, setJobData] = useState<JobDetails | null>(null);
+  const [orgData, setOrgData] = useState<OrgDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [isDeletingListing, setIsDeletingListing] = useState(false);
+  const [subgraphData, setSubgraphData] = useState<Applicant[]>();
+  const [sepoliaCount, setSepoliaCount] = useState(0);
+  const [optimismCount, setOptimismCount] = useState(0);
+  const [fujiCount, setFujiCount] = useState(0);
+  const [isClickApply, setIsClickApply] = useState(false);
+  const { address } = useAccount();
+  const router = useRouter();
+  const chainsOptions = ["Sepolia", "Optimism Goerli", "Avalanche Fuji"];
+  const [selectedChain, setSelectedChain] = useState("");
+
   useEffect(() => {
     const handleJobDetails = async () => {
       try {
@@ -168,66 +239,18 @@ function JobHeader({ listing_id }: Props) {
           setOrgData(orgData);
         }
 
-        // TODO: fetch all the applicants that applied to the same listing ID
+        // Fetch all data from subgraph based on listing id
+        const subgraphReturnedData: subgraphReturnedData =
+          await fetchAllApplicantFromSubgraph({
+            listing_id,
+          });
 
-        const listingData = await ChainInApi.fetchApplicationByListingId(
-          listing_id
-        );
-        setTotalApplicants(listingData.length);
+        setSepoliaCount(subgraphReturnedData.sepoliaCount);
+        setOptimismCount(subgraphReturnedData.optimismCount);
+        setFujiCount(subgraphReturnedData.fujiCount);
+        setSubgraphData(subgraphReturnedData.combinedData);
 
-        // TODO: fetch data from subgraph and calculate the total minted NFT on each destination chain
-
-        // let sepoliaResponse: subgraphResponse =
-        //   await sepoliaGraphQLClient.request(queryFormat);
-        // let opResponse: subgraphResponse = await opGoerGraphQLClient.request(
-        //   queryFormat
-        // );
-        // let fujiResponse: subgraphResponse = await fujiGraphQLClient.request(
-        //   queryFormat
-        // );
-
-        // console.log("sepolia response ", sepoliaResponse);
-        // console.log("op response ", opResponse);
-        // console.log("fuji response ", fujiResponse);
-        // const sepoliaApplicants = sepoliaResponse.data.newApplicants;
-
-        // const sepoliaListingIDCount: ListingIDCounts = {};
-
-        // sepoliaApplicants.forEach((applicant) => {
-        //   const listingID = applicant.listingID;
-        //   sepoliaListingIDCount[listingID] =
-        //     (sepoliaListingIDCount[listingID] || 0) + 1;
-        // });
-
-        // const fujiApplicants = fujiResponse.data.newApplicants;
-
-        // const fujiListingIDCount: ListingIDCounts = {};
-
-        // fujiApplicants.forEach((applicant) => {
-        //   const listingID = applicant.listingID;
-        //   fujiListingIDCount[listingID] =
-        //     (fujiListingIDCount[listingID] || 0) + 1;
-        // });
-
-        // const opApplicants = opResponse.data.newApplicants;
-
-        // const opListingIDCount: ListingIDCounts = {};
-
-        // opApplicants.forEach((applicant) => {
-        //   const listingID = applicant.listingID;
-        //   opListingIDCount[listingID] = (opListingIDCount[listingID] || 0) + 1;
-        // });
-
-        // setSepoliaApplicantCount(sepoliaListingIDCount[listing_id]);
-        // setOpApplicantCount(opListingIDCount[listing_id]);
-        // setFujiApplicantCount(opListingIDCount[listing_id]);
-        // let combinedApplicantData = [
-        //   ...sepoliaApplicants,
-        //   ...fujiApplicants,
-        //   ...opApplicants,
-        // ];
-        // setAllApplicants(combinedApplicantData);
-        // postNewApplicant(allApplicants!, combinedApplicantData);
+        postNewApplicant(subgraphData, subgraphReturnedData.combinedData);
       } catch (error) {
         console.error("Error fetching job data:", error);
       } finally {
@@ -235,7 +258,6 @@ function JobHeader({ listing_id }: Props) {
       }
     };
     handleJobDetails();
-    console.log("Conenct account from job header ", address);
   }, [listing_id]);
 
   const handleDeleteListing = async () => {
@@ -251,78 +273,6 @@ function JobHeader({ listing_id }: Props) {
     }
   };
 
-  const handleDeleteListing = async () => {
-    try {
-      setIsDeletingListing(true);
-      const listing = await ChainInApi.deleteListingByListingId(listing_id);
-      console.log("listing deleted", listing);
-      setIsDeletingListing(false);
-      router.push("/home");
-    } catch (error) {
-      console.error("Error deleting listing:", error);
-      setIsDeletingListing(false);
-    }
-  };
-
-  const openNewTab = (url: any) => {
-    window.open(url, "_blank");
-  };
-  const handleSelectedChain = (selectedChain: string) => {
-    console.log("Selected ", selectedChain);
-    console.log("Apply ", jobData?.results[0].listing_id);
-  };
-
-  const { config } = usePrepareContractWrite({
-    address: orgData?.results[0].nft_contract_address as `0x${string}`,
-    abi: [
-      {
-        name: "applyJob",
-        type: "function",
-        stateMutability: "nonpayable",
-        inputs: [
-          {
-            internalType: "uint64",
-            name: "destinationChainSelector",
-            type: "uint64",
-          },
-          {
-            internalType: "address",
-            name: "destinationMinter",
-            type: "address",
-          },
-          {
-            internalType: "bool",
-            name: "isPayLink",
-            type: "bool",
-          },
-          {
-            internalType: "uint256",
-            name: "listingID",
-            type: "uint256",
-          },
-          {
-            internalType: "string",
-            name: "profileURL",
-            type: "string",
-          },
-        ],
-        outputs: [],
-      },
-    ],
-    functionName: "applyJob",
-    args: [
-      returnDestinationInfo(selectedChain)?.selector,
-      returnDestinationInfo(selectedChain)?.minter,
-      true,
-      jobData?.results[0].listing_id,
-      `${jobData?.results[0].listing_id}/${address}`,
-    ],
-  });
-  const { data, write } = useContractWrite(config);
-
-  const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
-  });
   return (
     <div className="flex w-full flex-col justify-star profile-header mt-3">
       {loading ? (
@@ -355,95 +305,112 @@ function JobHeader({ listing_id }: Props) {
               <MapPinned />
               {jobData?.results[0].location}
             </p>
+
+            {sepoliaCount + fujiCount + optimismCount == 0 ? (
+              <p className="text-base mt-1 text-white flex gap-2">
+                <MapPinned />
+                Total Applicant: 0
+              </p>
+            ) : (
+              <p className="text-base mt-1 text-white flex gap-2">
+                <MapPinned />
+                Total Applicant: {sepoliaCount + fujiCount + optimismCount}
+                {sepoliaCount == 0
+                  ? `${sepoliaCount} member(s) applied to Sepolia `
+                  : ""}
+                {optimismCount == 0
+                  ? `${optimismCount} member(s) applied to Optimism Goerli `
+                  : ""}
+                {fujiCount == 0
+                  ? `${fujiCount} member(s) applied to Avalanche Fuji`
+                  : ""}
+              </p>
+            )}
+
             <p className="text-base mt-10 text-white flex flex-col w-full">
               <span className="font-bold text-lg">Job Description</span>
               {jobData?.results[0].description}
             </p>
-            <p className="text-base mt-10 text-white flex flex-col w-full">
-              <span className="font-bold text-lg">Total Applicants</span>
-              {totalApplicants}
-            </p>
-            {sepoliaApplicantCount != 0 ||
-              null ||
-              (undefined && (
-                <p className="text-base mt-10 text-white flex flex-col w-full">
-                  <span className="font-bold text-lg">
-                    Applied from Sepolia
-                  </span>
-                  {sepoliaApplicantCount}
-                </p>
-              ))}
-            {opApplicantCount != 0 ||
-              null ||
-              (undefined && (
-                <p className="text-base mt-10 text-white flex flex-col w-full">
-                  <span className="font-bold text-lg">
-                    Applied from Optimism Goerli
-                  </span>
-                  {opApplicantCount}
-                </p>
-              ))}
-            {fujiApplicantCount != 0 ||
-              null ||
-              (undefined && (
-                <p className="text-base mt-10 text-white flex flex-col w-full">
-                  <span className="font-bold text-lg">
-                    Applied from Avalanche Fuji
-                  </span>
-                  {fujiApplicantCount}
-                </p>
-              ))}
           </div>
           {jobData?.results[0].organisation_creator === address ? (
-            <Button
-              className="text-sm gap-2 mb-auto"
-              onClick={() => {
-                router.push(`/edit-job/${listing_id}`);
-              }}
-            >
-              <FileEdit />
-            </Button>
+            <div className="flex gap-2 mb-auto">
+              <Button
+                className="text-sm"
+                onClick={() => {
+                  router.push(`/edit-job/${listing_id}`);
+                }}
+              >
+                <FileEdit />
+              </Button>
+              <Dialog>
+                <DialogTrigger className="bg-[#FF6961] text-[#E6E6E6] hover:bg-[#E6E6E6]  hover:text-[#4A6FA4] text-lg font-bold rounded-2xl cursor-pointer h-10 px-4 py-2">
+                  <Trash2 />
+                </DialogTrigger>
+                <DialogContent className="bg-[#E6E6E6] border-none">
+                  <DialogHeader>
+                    <DialogTitle className="text-[#4A6FA4] flex items-center justify-center mb-4">
+                      Are you absolutely sure?
+                    </DialogTitle>
+                    <DialogDescription className="text-[#4A6FA4]">
+                      This action cannot be undone. This will permanently delete
+                      your job listing and remove the job listing's data from
+                      our servers.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex items-center mt-3">
+                    <DialogClose asChild>
+                      <Button className="text-sm hover:bg-[#ADBFDA]">
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button
+                      className="text-sm ml-auto bg-[#FF6961] gap-2 hover:bg-[#ADBFDA]"
+                      onClick={handleDeleteListing}
+                    >
+                      {isDeletingListing ? (
+                        <>
+                          Deleting
+                          <SyncLoader size={3} color="#E6E6E6" />
+                        </>
+                      ) : (
+                        "Confirm"
+                      )}
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
           ) : (
-            <Button
-              className="text-sm gap-2 mb-auto"
-              onClick={() => {
-                setIsClickApply(true);
-              }}
-            >
-              <Hand />
-              Apply for Job
-            </Button>
+            // Logic for normal user
+            <>
+              <Button
+                className="text-sm gap-2 mb-auto"
+                onClick={() => setIsClickApply(true)}
+              >
+                <Hand />
+                Apply for Job
+              </Button>
+
+              {isClickApply && (
+                <>
+                  <label htmlFor="dropdown">
+                    Select destination chain to mint the application NFT:
+                  </label>
+                  <select
+                    id="dropdown"
+                    onChange={(e) => setSelectedChain(e.target.value)}
+                  >
+                    {chainsOptions.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                </>
+              )}
+            </>
           )}
         </div>
-      )}
-      {isClickApply && (
-        <>
-          <ApplyJobCard
-            isOpen={isClickApply}
-            onClose={() => setIsClickApply(false)}
-            onSelect={handleSelectedChain}
-          />
-          <button
-            disabled={!write || isLoading}
-            onClick={() => {
-              // write?.();
-              console.log("Click");
-            }}
-            className="bg-[#0603c8] text-white"
-          >
-            {isLoading ? "Sending Application...." : "Send Application"}
-          </button>
-          {isSuccess && (
-            <div>
-              Successfully submitted your NFT!
-              <div>
-                <a href={`https://mumbai.polygonscan.com/tx/${data?.hash}`}>
-                  Etherscan
-                </a>
-              </div>
-            </div>
-          )}
-        </>
       )}
       <div className="mt-12 h-0.5 w-full bg-dark-3" />
     </div>
